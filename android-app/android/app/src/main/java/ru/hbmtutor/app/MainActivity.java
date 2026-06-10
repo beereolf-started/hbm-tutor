cca86c3c21cd728fca7d78d6a6da69392858f2e8
beereolf-started <beereolf@gmail.com>
2026-06-10 03:57:28 +0300

    Add Capacitor Android wrapper for hbmtutor.ru with CI APK build
    WebView wrapper pointing at https://hbmtutor.ru, with camera/mic
    permissions wired up for WebRTC calls. GitHub Actions workflow
    builds a debug APK on push, since local builds are blocked by a
    Windows/VPN loopback networking issue with Java NIO.

---
+++ android-app/android/app/src/main/java/ru/hbmtutor/app/MainActivity.java
@@ -0,0 +1,28 @@
+package ru.hbmtutor.app;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import androidx.core.app.ActivityCompat;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebChromeClient;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+        }, 1);
+
+        getBridge().getWebView().setWebChromeClient(new BridgeWebChromeClient(getBridge()) {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                runOnUiThread(() -> request.grant(request.getResources()));
+            }
+        });
+    }
+}
